96f375e93e4aebea3a07c3783fb263e02ffeb036
Myllene Mendes <97467712+myllenefms@users.noreply.github.com>
2023-12-22 00:28:28 +0000

    build/create app

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
+from plot.interactive import plot_line_i as pli
 import streamlit as st
-#import yfinance as yf
-import pandas as pd
-from data.download import download_data
-from plot.static import plot_line
-from plotnine import *
 
-st.title('Stock Price App')
+st.title('Preços de Ações App')
 
 # Sidebar
-st.sidebar.header('User Input')
-symbol = st.sidebar.text_input('Enter stock symbol (e.g., AAPL):', 'AAPL')
-
-stock_data = download_data(symbol)
-
-st.subheader(f'Stock Data for {symbol}')
-st.write(stock_data)
-
-# fig = ggplot(
-#     data = stock_data.reset_index(),
-#     mapping = aes(x = 'Date', y  = 'Close')
-# ) + \
-#     geom_line() + \
-#     ggtitle(f'Dados Históricos do {symbol}') + \
-#     xlab('Data') + \
-#     ylab('Fechamento')
-stock_data = pd.DataFrame(stock_data)
-fig = plot_line(stock_data)
-#st.subheader('Stock Price Chart')
-st.pyplot(fig)
+st.sidebar.header('Consulta')
+symbol = st.sidebar.text_input('Digite um ativo:', 'AAPL')
+st.pyplot(pli(symbol))
--- rascunho.py
+++
@@ -1,5 +0,0 @@
-import plotly.express as px
-from data.download import download_data
-from plot.interactive import plot_line_i
-
-plot_line_i("BBAS3.SA")
